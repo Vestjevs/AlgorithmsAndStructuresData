a9c91e29d88f95e5b26028420d0dd7ea05ebda2c
vestjevs <ac.lbear@yandex.ru>
2018-09-15 16:10:33 +0300

    without main, not QS

---
+++ src/USearch/UF.java
@@ -0,0 +1,42 @@
+package USearch;
+
+
+
+public class UF {
+    private int[] id; // access to the component identifier
+    private int count;
+
+    UF(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID == qID) return;
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pID) id[i] = qID;
+            count--;
+        }
+
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int count() {
+        return count;
+    }
+
+}
